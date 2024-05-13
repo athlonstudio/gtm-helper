1da6a6bff9bc21a5c4bf8ffa95a79e3515accf0f
JJ Bastida <162381441+jj-athlon@users.noreply.github.com>
2024-05-13 16:40:05 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
 const cookieKey = document.currentScript.dataset.cookieKey;
-const cookiesApproved = document.cookie.split('; ').map((cookie) => cookie.includes(cookieKey) || cookie.includes(cookieKey)).includes(true);
+const cookiesApproved = document.cookie.split('; ').map((cookie) => cookie.includes(`${cookieKey}="all"`) || cookie.includes(`${cookieKey}="all"`)).includes(true);
 const GTMkey = document.currentScript.dataset.gtm;
 const GAkey = document.currentScript.dataset.ga;
 
 function enableGTM() {
-  window['dataLayer'] = window['dataLayer']||[];
-  window['dataLayer'].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-  const location = document.getElementsByTagName('script')[0],elem=document.createElement('script'),dl='dataLayer'!='dataLayer'?'&l='+'dataLayer':'';
-  elem.async=true;elem.src='https://www.googletagmanager.com/gtm.js?id='+GTMkey+dl;
-  location.parentNode.insertBefore(elem,location);
+  if(GTMKey) {
+    window['dataLayer'] = window['dataLayer']||[];
+    window['dataLayer'].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+    const location = document.getElementsByTagName('script')[0],elem=document.createElement('script'),dl='dataLayer'!='dataLayer'?'&l='+'dataLayer':'';
+    elem.async=true;elem.src='https://www.googletagmanager.com/gtm.js?id='+GTMkey+dl;
+    location.parentNode.insertBefore(elem,location);
+  }
 }
 
 function enableGA(){
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
-  gtag('event', 'conversion_event_page_view_1', {});
-  gtag('config', GAkey);
+  if(GAkey) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)}
+    gtag('js', new Date());
+    gtag('event', 'conversion_event_page_view_1', {});
+    gtag('config', GAkey);
+  }
 }
 
 
@@ -26,7 +30,7 @@ function initiateCookie() {
   
   exipiryDate.setTime(exipiryDate.getTime() + (monthInMilliseconds * 3));
   exipiryDate.toUTCString();
-  document.cookie = `${cookieKey}; expires=${exipiryDate}`;
+  document.cookie = `${cookieKey}=all; expires=${exipiryDate}`;
   document.querySelector('[fs-cc="banner"]').remove();
   
   enableGTM();
