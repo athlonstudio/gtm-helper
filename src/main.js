ea4b474135f1165ede78bd1b95ef737ef7975b91
JJ Bastida <162381441+jj-athlon@users.noreply.github.com>
2025-04-03 15:04:50 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,43 +1,8 @@
-const cookieKey = document.currentScript.dataset.cookieKey;
-const cookiesApproved = document.cookie.split('; ').map((cookie) => cookie.includes(`${cookieKey}=all`) || cookie.includes(`${cookieKey}=all`)).includes(true);
-const GTMkey = document.currentScript.dataset.gtm;
-const GAkey = document.currentScript.dataset.ga;
-
-function enableGTM() {
-  if(GTMkey) {
-    window['dataLayer'] = window['dataLayer']||[];
-    window['dataLayer'].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-    const location = document.getElementsByTagName('script')[0],elem=document.createElement('script'),dl='dataLayer'!='dataLayer'?'&l='+'dataLayer':'';
-    elem.async=true;elem.src='https://www.googletagmanager.com/gtm.js?id='+GTMkey+dl;
-    location.parentNode.insertBefore(elem,location);
-  }
-}
-
-function enableGA(){
-  if(GAkey) {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments)}
-    gtag('js', new Date());
-    gtag('event', 'conversion_event_page_view_1', {});
-    gtag('config', GAkey);
-  }
-}
-
-
-function initiateCookie() {
-  const exipiryDate = new Date();
-  const monthInMilliseconds = 30 * 86400000;
-  
-  exipiryDate.setTime(exipiryDate.getTime() + (monthInMilliseconds * 3));
-  exipiryDate.toUTCString();
-  document.cookie = `${cookieKey}=all; expires=${exipiryDate}`;
-  document.querySelector('[fs-cc="banner"]').remove();
-  
-  enableGTM();
-  enableGA();
-}
-
-document.addEventListener("DOMContentLoaded", () => {
+function initializeGTMHelper() {
+  const cookieKey = document.currentScript.dataset.cookieKey;
+  const cookiesApproved = document.cookie.split('; ').map((cookie) => cookie.includes(`${cookieKey}=all`) || cookie.includes(`${cookieKey}=all`)).includes(true);
+  const GTMkey = document.currentScript.dataset.gtm;
+  const GAkey = document.currentScript.dataset.ga;
   const cookieBanner = document.querySelector('[fs-cc="banner"]');
   
   if(!cookiesApproved && cookieKey && cookieBanner){
@@ -50,5 +15,41 @@ document.addEventListener("DOMContentLoaded", () => {
       cookieBanner.remove();
      }
   }
+  
+  function enableGTM() {
+    if(GTMkey) {
+      window['dataLayer'] = window['dataLayer']||[];
+      window['dataLayer'].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+      const location = document.getElementsByTagName('script')[0],elem=document.createElement('script'),dl='dataLayer'!='dataLayer'?'&l='+'dataLayer':'';
+      elem.async=true;elem.src='https://www.googletagmanager.com/gtm.js?id='+GTMkey+dl;
+      location.parentNode.insertBefore(elem,location);
+    }
+  }
+  
+  function enableGA(){
+    if(GAkey) {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)}
+      gtag('js', new Date());
+      gtag('event', 'conversion_event_page_view_1', {});
+      gtag('config', GAkey);
+    }
+  }
+  
+  
+  function initiateCookie() {
+    const exipiryDate = new Date();
+    const monthInMilliseconds = 30 * 86400000;
+    
+    exipiryDate.setTime(exipiryDate.getTime() + (monthInMilliseconds * 3));
+    exipiryDate.toUTCString();
+    document.cookie = `${cookieKey}=all; expires=${exipiryDate}`;
+    document.querySelector('[fs-cc="banner"]').remove();
+    
+    enableGTM();
+    enableGA();
+  }
+}
 
-});
+
+document.addEventListener("DOMContentLoaded", () => initializeGTMHelper);
